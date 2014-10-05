c59fac2415dd06f81b41ab2044a826f35de82aa2
Matthew Pineau <matt.w.pineau@gmail.com>
2014-10-04 21:48:25 -0400

    Create file limbo.sql and enter some commands (tasks 1-4)

---
+++ limbo.sql
@@ -0,0 +1,11 @@
+--Assignment 2 for Software Development II--
+--Version: 1.0--
+--Authors: Matt Pineau, Siting Wang--
+--Date: 8/4/14--
+
+delete database if exists limbo_db;
+create database if not exists limbo_db;
+use limbo_db;
+
+create table if not exists users;
+
