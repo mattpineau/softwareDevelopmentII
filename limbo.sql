0dd6ffedd06b7a9bea11793e433614413b94a7d3
Matthew Pineau <matt.w.pineau@gmail.com>
2014-10-09 21:10:48 -0400

    Finalize assignment to be handed in

--- limbo.sql
+++ limbo.sql
@@ -1,26 +1,31 @@
---Assignment 2 for Software Development II--
---Version: 1.0--
---Authors: Matt Pineau, Siting Wang--
---Date: 8/4/14--
+#Assignment 2 for Software Development II
+#This file will: 
+#                create a database- limbo_db
+#                create tables- users, stuff, locations
+#                populate tables- users, locations
 
-delete database if exists limbo_db;
+#Version: 1.0
+#Authors: Matt Pineau, Siting Wang
+#Date: 8/4/14
+
+drop database if exists limbo_db;
 create database if not exists limbo_db;
 use limbo_db;
 
-create table if not exists users(
+create table if not exists users (
 user_id int unsigned not null auto_increment, 
 first_name varchar(20) not null, 
 last_name varchar(40) not null, 
-email varchar(60) not null, 
+email varchar(60) not null unique, 
 pass char(40) not null, 
 reg_date datetime not null, 
-primary key (user_id), 
-unique (email) );
+primary key (user_id)
+);
 
-insert into users (first_name, pass)
-values ("admin", "gazelle");
+insert into users (first_name, pass) 
+values ('admin', 'gaze11e');
 
-create table if not exists stuff(
+create table if not exists stuff (
 stuff_id int auto_increment, 
 location_id int not null, 
 description text not null, 
@@ -30,48 +35,51 @@ room text,
 owner text, 
 finder text, 
 status set ("found", "lost", "claimed") not null, 
-primary key (stuff_id));
+primary key (stuff_id)
+);
 
-create table if not exists locations(
+create table if not exists locations (
 location_id int auto_increment, 
-create_date datetime not null, 
-update_date datetime not null, 
+create_date datetime not null default now(), 
+update_date datetime not null default now(), 
 name text not null,
-primary key (location_id));
+primary key (location_id)
+);
 
-insert into locations (create_date, update_date, name)
-values (Now(), Now(),"Byrne House"), 
-       (Now(), Now(),"Our Lady Seat of Wsdom Chapel"), 
-       (Now(), Now(),"Champagnat Hall"), 
-       (Now(), Now(),"Cornell Boathouse"), 
-       (Now(), Now(),"Donnelly Hall"), 
-       (Now(), Now(),"Dyson Center"), 
-       (Now(), Now(),"Fern Tor"), 
-       (Now(), Now(),"Fontaine Annex"), 
-       (Now(), Now(),"Fontaine Hall"), 
-       (Now(), Now(),"Foy Townhouses"), 
-       (Now(), Now(),"Fulton Street Townhouses"), 
-       (Now(), Now(),"New Fulton Townhouses"), 
-       (Now(), Now(),"Gartland Commons"), 
-       (Now(), Now(),"Greystone Hall"), 
-       (Now(), Now(),"Hancock Center"), 
-       (Now(), Now(),"Kieran Gatehouse"), 
-       (Now(), Now(),"Kirk House"), 
-       (Now(), Now(),"Leo Hall"), 
-       (Now(), Now(),"James A. Cannavino Library"), 
-       (Now(), Now(),"Longview Park"), (Now(), Now(),"Lowell Thomas Communications Center"), 
-       (Now(), Now(),"Lower Townhouses"), 
-       (Now(), Now(),"Marian Hall"), 
-       (Now(), Now(),"Marist Boathouse"), 
-       (Now(), Now(),"James J. McCann Recreational Center"), 
-       (Now(), Now(),"Midrise Hall"), 
-       (Now(), Now(),"New Townhouses"), 
-       (Now(), Now(),"St. Ann's Hermitage"), 
-       (Now(), Now(),"St. Peter's"), 
-       (Now(), Now(),"Sheahan Hall"), 
-       (Now(), Now(),"Steel Plant Art Studios"), 
-       (Now(), Now(),"Student Center/Rotunda"), 
-       (Now(), Now(),"Tenney Stadium"), 
-       (Now(), Now(),"Tennis Pavillion"), 
-       (Now(), Now(),"Lower West Cedar Townhouses") 
-       (Now(), Now(),"Upper West Cedar Townhouses");
+insert into locations (name)
+values ('Byrne House'), 
+       ('Our Lady Seat of Wisdom Chapel'), 
+       ('Champagnat Hall'), 
+       ('Cornell Boathouse'), 
+       ('Donnelly Hall'), 
+       ('Dyson Center'), 
+       ('Fern Tor'), 
+       ('Fontaine Annex'), 
+       ('Fontaine Hall'), 
+       ('Foy Townhouses'), 
+       ('Fulton Street Townhouses'), 
+       ('New Fulton Townhouses'), 
+       ('Gartland Commons'), 
+       ('Greystone Hall'), 
+       ('Hancock Center'), 
+       ('Kieran Gatehouse'), 
+       ('Kirk House'), 
+       ('Leo Hall'), 
+       ('James A. Cannavino Library'), 
+       ('Longview Park'), 
+       ('Lowell Thomas Communications Center'), 
+       ('Lower Townhouses'), 
+       ('Marian Hall'), 
+       ('Marist Boathouse'), 
+       ('James J. McCann Recreational Center'), 
+       ('Midrise Hall'), 
+       ('New Townhouses'), 
+       ('St. Ann\'s Hermitage'), 
+       ('St. Peter\'s'), 
+       ('Sheahan Hall'), 
+       ('Steel Plant Art Studios'), 
+       ('Student Center/Rotunda'), 
+       ('Tenney Stadium'), 
+       ('Tennis Pavillion'), 
+       ('Lower West Cedar Townhouses'), 
+       ('Upper West Cedar Townhouses');
